7211ac511ec520f56fdaff3331cf8e7e439dc4c8
Maxwell Chen <maxhchen@gmail.com>
2020-06-26 22:44:03 +0000

    Pixelation works on demo image. Animated transition between desired numbers of pixels.

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,58 +1,139 @@
-// window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame;
+// Pixelate function: (C) Ken Fyrstenberg, Epistemex, License: CC3.0-attr
+var ctx = canvas.getContext('2d');
+var animated = false;
 
-var image = document.querySelector('.image');
-var pixelation = new Pixelate(image);
+// turn off image smoothing for pixelated effect
+ctx.mozImageSmoothingEnabled = false;
+ctx.webkitImageSmoothingEnabled = false;
+ctx.imageSmoothingEnabled = false;
 
-var slider = document.querySelector('.slider');
-var output = document.getElementById('output');
+var img = document.getElementById('species-image');
 
-slider.addEventListener('input', function(event) {
-  var amount = event.currentTarget.value;
-  update(amount);
-});
+// var originalNumPixels = img.width * img.height;
+var originalNumPixels = 100;
 
-function update(amount) {
-  output.textContent = Math.round(amount) + '%';
-  pixelation.setAmount(amount / 100).render();
+// wait until image is actually available
+img.onload = pixelate;
+
+// Pixelation function
+// Shrinks image and then scales it back up w/o smoothing for automatic pixelation
+// v = 100 is original image quality
+function pixelate(v) {
+
+        // Use input value; v = percentage to scale dimensions down to
+    // var size = v * 0.01;
+        // cache scaled width and height
+        // w = # pixels wide
+        // h = # pixels tall
+    // w = canvas.width * size,
+    // h = canvas.height * size;
+
+    var lambda = animated? v : pixel_factor.value;
+
+    var w = Math.sqrt(lambda);
+    var h = Math.sqrt(lambda);
+
+    console.log(lambda, w, h);
+
+    // draw original image to the scaled size
+    ctx.drawImage(img, 0, 0, w, h);
+
+    // then draw that scaled image thumb back to fill canvas
+    // As smoothing is off the result will be pixelated
+    ctx.drawImage(canvas, 0, 0, w, h, 0, 0, canvas.width, canvas.height);
 }
 
-// window.onresize = function() {
-//   pixelation.setWidth(image.parentNode.clientWidth).render();
-// };
+var PIXEL_FACTOR_CURR = originalNumPixels;
 
-update(slider.value)
+function animate_update(endpoint) {
+    var target = pixel_factor.value;
+    var dx = 10;
+    animated = true;
+    var underTarget = false;
+    doAnimation();
+    function doAnimation() {
 
-    // Method 2: Manual pixelation
+        if (PIXEL_FACTOR_CURR < target) {
+            PIXEL_FACTOR_CURR += dx;
+            if (!underTarget) {
+                dx -= 1;
+            }
+            underTarget = true;
+        } else if (PIXEL_FACTOR_CURR > target) {
+            PIXEL_FACTOR_CURR -= dx;
+            if (underTarget) {
+                dx -= 1;
+            }
+            underTarget = false;
+        } else {
+            animated = false;
+            return;
+        }
 
-    // let c = document.createElement("canvas");
-    // ctx = c.getContext('2d');
-    // let img1 = new Image();
+        pixelate(PIXEL_FACTOR_CURR);
 
-    // img1.onload = function () {
-    //   document.getElementById("image1").remove();
+        if (animated) {
+            requestAnimationFrame(doAnimation);
+        }
+    }
+}
 
-    //   w = img1.width;
-    //   h = img1.height;
+// Restore pixelation.
+function out() {
+    var v = originalNumPixels;
+    var dx = 10; // "speed" parameter
+    console.log("max pixelation: v starts at " + v);
 
-    //   c.width = w;
-    //   c.height = h;
-    //   ctx.drawImage(img1, 0, 0);
+    animated = true;
+    anim();
 
-    //   var pixelArr = ctx.getImageData(0, 0, w, h).data;
-    //   sample_size = 128;
+    function anim() {
+        v -= dx;
 
-    //   for (let y = 0; y < h; y += sample_size) {
-    //     for (let x = 0; x < w; x += sample_size) {
-    //       let p = (x + (y*w)) * 4;
-    //       ctx.fillStyle = "rgba(" + pixelArr[p] + "," + pixelArr[p + 1] + "," + pixelArr[p + 2] + "," + pixelArr[p + 3] + ")";
-    //       ctx.fillRect(x, y, sample_size, sample_size);
-    //     }
-    //   }
+        if (v < PIXEL_FACTOR_CURR) {
+            animated = false;
+            return;
+        }
 
-    //   let img2 = new Image();
-    //   img2.src = c.toDataURL("image/jpeg");
-    //   img2.width = 600;
-    //   document.body.appendChild(img2);
-    // };
+        pixelate(v);
 
-    // img1.src = document.getElementById("image1").src;
+        if (animated) {
+            requestAnimationFrame(anim);
+        }
+    }
+}
+
+// Full resolution
+function over() {
+    var v = PIXEL_FACTOR_CURR;
+    var dx = 10; // "speed" parameter
+    console.log("go to original: v starts at " + v);
+
+    animated = true;
+    anim();
+
+    function anim() {
+        v += dx;
+
+        if (v > originalNumPixels) {
+            animated = false;
+            return;
+        }
+
+        pixelate(v);
+        
+        if (animated) {
+            requestAnimationFrame(anim);
+        }
+    }
+}
+
+pixel_factor.addEventListener('change', animate_update, false);
+
+// poly-fill for requestAnmationFrame with fallback for older
+// browsers which do not support rAF.
+// window.requestAnimationFrame = (function () {
+//     return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function (callback) {
+//         window.setTimeout(callback, 1000 / 60);
+//     };
+// })();
