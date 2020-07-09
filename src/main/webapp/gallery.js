dae3addc304df10ec03b714eb3a06fd9a20723e2
Ariana Zhu <zhu811@purdue.edu>
2020-07-09 22:23:32 +0000

    Enabled dyamic image gallery

--- src/main/webapp/gallery.js
+++ src/main/webapp/gallery.js
@@ -7,12 +7,39 @@ var $grid = $('.grid').masonry({
     itemSelector: '.grid-item',
     percentPosition: true,
     columnWidth: '.grid-sizer',
-    horizontalOrder: true,
-    fitWidth: true
+    horizontalOrder: true
 });
+
+// Load JSON file
+var data = JSON.parse(animals-test);
+console.log(data);
+
+var imageArray = [
+    "test-images/deer.jpg",
+    "test-images/giraffe.jpg",
+    "test-images/owl.jpg",
+    "test-images/pangolin.jpg",
+    "test-images/snake.jpg",
+    "test-images/squirrel.jpeg",
+    "test-images/tiger.jpeg",
+    "test-images/toucan.jpg",
+    "test-images/peacock.jpeg",
+    "test-images/porcupine.jpg",
+    "test-images/red-panda.png",
+    "test-images/seal.jpg"
+];
+
+for (i = 0; i < imageArray.length; i++) {
+    // Append images to grid
+    var $html = $('<div class="grid-item"> <img src="'+ imageArray[i] + '" /> </div>'); 
+    $grid.append($html)
+        // add and lay out newly appended items
+        .masonry('appended', $html);
+}
 
 // Layout Masonry after each image finishes loading
 $grid.imagesLoaded().progress( function() {
     $grid.masonry('layout');
 });
+
 
