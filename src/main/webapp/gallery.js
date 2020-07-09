11e86bac9e637b9f0d779e14bb10360bc20d5a8f
Ariana Zhu <zhu811@purdue.edu>
2020-07-09 23:31:58 +0000

    Added test JSON file to populate gallery

--- src/main/webapp/gallery.js
+++ src/main/webapp/gallery.js
@@ -3,6 +3,7 @@
 // Masonry is released under the MIT license: https://desandro.mit-license.org/
 
 // Initialize Masonry
+console.log("initializing masonry");
 var $grid = $('.grid').masonry({
     itemSelector: '.grid-item',
     percentPosition: true,
@@ -10,36 +11,24 @@ var $grid = $('.grid').masonry({
     horizontalOrder: true
 });
 
-// Load JSON file
-var data = JSON.parse(animals-test);
-console.log(data);
+fetchSpeciesData();
 
-var imageArray = [
-    "test-images/deer.jpg",
-    "test-images/giraffe.jpg",
-    "test-images/owl.jpg",
-    "test-images/pangolin.jpg",
-    "test-images/snake.jpg",
-    "test-images/squirrel.jpeg",
-    "test-images/tiger.jpeg",
-    "test-images/toucan.jpg",
-    "test-images/peacock.jpeg",
-    "test-images/porcupine.jpg",
-    "test-images/red-panda.png",
-    "test-images/seal.jpg"
-];
 
-for (i = 0; i < imageArray.length; i++) {
-    // Append images to grid
-    var $html = $('<div class="grid-item"> <img src="'+ imageArray[i] + '" /> </div>'); 
-    $grid.append($html)
-        // add and lay out newly appended items
-        .masonry('appended', $html);
+// Test function that fetches sample JSON and appends each species to the gallery
+function fetchSpeciesData() {
+    fetch("test.json").then(response => response.json()).then(speciesData => {
+        for (var species in speciesData) {
+            // Append images to grid
+            var $html = $('<div class="grid-item"> <img src="'+ speciesData[species].imageLink +
+                          '" /> <div class="overlay">' + speciesData[species].commonName + '</div> </div>'); 
+            $grid.append($html)
+                // add and lay out newly appended items
+                .masonry('appended', $html);
+        }
+
+        // Layout Masonry after each image finishes loading
+        $grid.imagesLoaded().progress( function() {
+            $grid.masonry('layout');
+        });
+    });
 }
-
-// Layout Masonry after each image finishes loading
-$grid.imagesLoaded().progress( function() {
-    $grid.masonry('layout');
-});
-
-
