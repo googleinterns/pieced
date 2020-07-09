0dbdf6af4bfa8be7d60fa49c01bc55542da80f17
Fiza <fgoyal2@illinois.edu>
2020-07-09 22:38:10 +0000

    Design nav bar with hovers

--- src/main/webapp/scripts/script.js
+++ src/main/webapp/scripts/script.js
@@ -1,4 +1,30 @@
-// Load navigation bar
-$(function() {
-    $(".navigation").load("navigation.html");
+$(document).ready(function() {
+  // executes when HTML-Document is loaded and DOM is ready
+
+
+  /*
+  ################
+  Add navbar background color when scrolled
+  */
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 56) {
+      $(".navbar").addClass("bg-nav");
+    } else {
+      $(".navbar").removeClass("bg-nav");
+    }
+  });
+  // If Mobile, add background color when toggler is clicked
+  $(".navbar-toggler").click(function() {
+    if (!$(".navbar-collapse").hasClass("show")) {
+      $(".navbar").addClass("bg-nav");
+    } else {
+      if ($(window).scrollTop() < 56) {
+        $(".navbar").removeClass("bg-nav");
+      } else {
+      }
+    }
+  });
+  // ############
+
+  // document ready
 });
