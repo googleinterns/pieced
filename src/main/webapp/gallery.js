300f41668e6aa2d432068bf8f070301de68f9079
Ariana Zhu <zhu811@purdue.edu>
2020-07-09 07:09:24 +0000

    Created responsive gallery view

--- src/main/webapp/gallery.js
+++ src/main/webapp/gallery.js
@@ -1,13 +1,18 @@
 // Played around with the following packages: masonry.pkgd.js, imagesloaded.pkgd.js
 // May need to use different libraries due to commercial use requirement
+// Masonry is released under the MIT license: https://desandro.mit-license.org/
 
 // Initialize Masonry
 var $grid = $('.grid').masonry({
-  itemSelector: '.grid-item',
-  percentPosition: true,
-  columnWidth: '.grid-sizer'
+    itemSelector: '.grid-item',
+    percentPosition: true,
+    columnWidth: '.grid-sizer',
+    horizontalOrder: true,
+    fitWidth: true
 });
+
 // Layout Masonry after each image finishes loading
 $grid.imagesLoaded().progress( function() {
-  $grid.masonry();
-});  
+    $grid.masonry('layout');
+});
+
