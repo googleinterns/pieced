ec4251623bf911857fc676048eb2139c33ef71e6
Ariana Zhu <zhu811@purdue.edu>
2020-07-14 22:54:20 +0000

    Added linking to individual species page from gallery

--- src/main/webapp/scripts/gallery.js
+++ src/main/webapp/scripts/gallery.js
@@ -18,8 +18,8 @@ function fetchAllSpeciesData() {
     fetch("/allData").then(response => response.json()).then(speciesData => {
         for (var species in speciesData) {
             // Append images to grid
-            var $html = $('<div class="grid-item"> <img src="'+ speciesData[species].imageLink +
-                          '" /> <div class="overlay">' + speciesData[species].commonName + '</div> </div>'); 
+            var $html = $('<div class="grid-item"> <a href="/species-template.html?species=' + speciesData[species].commonName + '"> <img src="'+ speciesData[species].imageLink +
+                          '" /> <div class="overlay">' + speciesData[species].commonName + '</div> </a> </div>'); 
             $grid.append($html)
                 // add and lay out newly appended items
                 .masonry('appended', $html);
---
+++ src/main/webapp/scripts/species-template.js
@@ -0,0 +1,9 @@
+// Get query parameters
+var urlParams = new URLSearchParams(window.location.search);
+if (urlParams.has('species')) {
+    const species = urlParams.get('species');
+    fetchSpeciesData(species);
+}
+else {
+    console.log("Error: no species was passed.");
+}
