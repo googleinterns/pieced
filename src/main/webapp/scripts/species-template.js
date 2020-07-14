fa6effed3ca8d919bc38c92db02585e38c9e1331
Ariana Zhu <zhu811@purdue.edu>
2020-07-14 23:07:24 +0000

    Minor changes

--- src/main/webapp/scripts/species-template.js
+++ src/main/webapp/scripts/species-template.js
@@ -2,8 +2,11 @@
 var urlParams = new URLSearchParams(window.location.search);
 if (urlParams.has('species')) {
     const species = urlParams.get('species');
+    document.title += "Pieced - " + species;
     fetchSpeciesData(species);
 }
 else {
     console.log("Error: no species was passed.");
 }
+
+//TODO: capitalize species name, fix general formatting
