280bb237e41ee1b6dd0a1bce25a89d1f89a53246
Fiza <fgoyal2@illinois.edu>
2020-08-01 07:46:20 +0000

    Updated home page design with cover photo + hover nav bar

--- src/main/webapp/scripts/script.js
+++ src/main/webapp/scripts/script.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
     // Load navigation bar
-    $(function() {
-        $("#navigation").load("navigation.html");
+    $("#navigation").load("navigation.html");
+    $('.header').height($(window).height());
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 10 ) {
+            $('.navbar').addClass('bg-nav');
+        } else {
+            $('.navbar').removeClass('bg-nav');
+        }
     });
 });
